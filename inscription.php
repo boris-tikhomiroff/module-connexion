a54d19bdbf9fd1b470e4e655a28fef6122f523bf
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-18 13:24:12 +0100

    verif login déja existant fonctionne

--- inscription.php
+++ inscription.php
@@ -4,13 +4,11 @@ session_start();
 
 // Connexion à la BDD
 $connect = mysqli_connect('localhost', 'root','','moduleconnexion');
-
+mysqli_set_charset($connect, 'utf8');
 //Vérifie la connexion à la BDD
 if($connect === false){
-    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+    die("ERREUR : Impossible de se connecter à la Base de données. " . mysqli_connect_error());
 }
-
-
 
 // Stock les valeurs renseignées par l'utilisateurs
 if(isset($_POST['inscription']))
@@ -19,15 +17,42 @@ if(isset($_POST['inscription']))
     $prenom = htmlspecialchars($_POST['user_prenom']);
     $nom = htmlspecialchars($_POST['user_nom']);
     $password = htmlspecialchars($_POST['user_password']);
-    $passworCheck = htmlspecialchars($_POST['user_password_check']);
-    
-    // Requete SQL
-    $query = "INSERT INTO `utilisateurs`(`login`, `prenom`, `nom`, `password`) VALUES ('$login','$prenom','$nom', '$password')";
-    
-    // Exécute la requête sur la base de données
-    $res = mysqli_query($connect, $query);
-}
+    $passwordCheck = htmlspecialchars($_POST['user_password_check']);    
 
+
+    
+    // Si login n'est pas renseigné, afficher une erreur.
+    if(empty($login)){
+        $loginError = "Veuillez renseigner votre login";
+        echo $loginError;
+    }
+    //Sinon si le login est renseigné, cherche dans la BDD si il existe déja.
+    elseif(!empty($login)){
+        $queryLogin = "SELECT `login`FROM `utilisateurs` WHERE `login` = '$login'";
+        $result = mysqli_query($connect, $queryLogin);
+        if(mysqli_num_rows($result) > 0){
+            $loginNotAvailable = "Le login n'est pas disponible";
+            echo $loginNotAvailable;
+        }
+    }
+    //Si prenom, nom, mdp et mdp pas renseigné erreur
+    elseif(empty($prenom) || empty($nom) || empty($password) || empty($passwordCheck)){
+        $missingError = "Veuillez rensenseigner ce champs";
+        echo $missingError;
+    }
+
+
+
+    // Si tu passes tout les tests, envoi la requete d'ajout utilisateur
+    else {
+        // // Coditions pour que la requete soit effectué
+    // if($password == $passwordCheck){
+    //     // Requete SQL
+    //     $query = "INSERT INTO `utilisateurs`(`login`, `prenom`, `nom`, `password`) VALUES ('$login','$prenom','$nom', '$password')";
+    //     $requestAddUser = mysqli_query($connect, $query);
+    // }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,15 +65,15 @@ if(isset($_POST['inscription']))
 <body>
     <form method="post" action="">
         <label for="login">Login :</label>
-        <input type="text" id="login" name="user_login" required>
+        <input type="text" id="login" name="user_login">
         <label for="prenom">Prenom :</label>
-        <input type="text" id="prenom" name="user_prenom" required>
+        <input type="text" id="prenom" name="user_prenom">
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="user_nom" required>
+        <input type="text" id="nom" name="user_nom">
         <label for="mdp">Mot de passe :</label>
-        <input type="password" id="mdp" name="user_password" required>
+        <input type="password" id="mdp" name="user_password">
         <label for="mdp2">Confirmez votre mot de passe :</label>
-        <input type="password" id="mdp2" name="user_password_check" required>
+        <input type="password" id="mdp2" name="user_password_check">
         <button type="submit" name="inscription">Inscription</button>
     </form>
 </body>
